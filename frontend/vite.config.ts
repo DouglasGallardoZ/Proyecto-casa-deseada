a02e947be3ee5ba0c1f438f32e29c60ea727631a
Douglas Gallardo <douglasgallardoz11@gmail.com>
2023-12-10 23:29:45 -0500

    Ajuste de imagenes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import copy from 'vite-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-  //  copy({
-  //    targets: [
-  //      { src: 'resources/**/*', dest: 'dist/' },
-  //    ],
-  //  }),
   ],
 })
