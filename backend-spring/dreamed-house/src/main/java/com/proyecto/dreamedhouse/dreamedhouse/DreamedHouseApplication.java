71bfadeb41f420ce10e308380a9237ecae244f29
Douglas Gallardo <douglasgallardoz11@gmail.com>
2024-01-10 22:50:41 -0500

    Se agrega Readme en backend

--- backend-spring/dreamed-house/src/main/java/com/proyecto/dreamedhouse/dreamedhouse/DreamedHouseApplication.java
+++ backend-spring/dreamed-house/src/main/java/com/proyecto/dreamedhouse/dreamedhouse/DreamedHouseApplication.java
@@ -2,8 +2,10 @@ package com.proyecto.dreamedhouse.dreamedhouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 public class DreamedHouseApplication {
 
 	public static void main(String[] args) {
