8f3fc17e3e8cc98cd5820a4bce0e6fbafb6a687f
Douglas Gallardo <douglasgallardoz11@gmail.com>
2024-01-10 23:05:57 -0500

    Se agrega Readme en backend

--- backend-spring/dreamed-house/src/main/java/com/proyecto/dreamedhouse/dreamedhouse/WebConfig.java
+++ backend-spring/dreamed-house/src/main/java/com/proyecto/dreamedhouse/dreamedhouse/WebConfig.java
@@ -1,5 +1,7 @@
 package com.proyecto.dreamedhouse.dreamedhouse;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -17,5 +19,13 @@ public class WebConfig implements WebMvcConfigurer{
                 .allowedHeaders("*")
                 .allowCredentials(false)
                 .maxAge(3600);
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CorsFilter());
+        registrationBean.addUrlPatterns("/api/*"); // Ajusta esto según tus necesidades
+        return registrationBean;
     }
 }
